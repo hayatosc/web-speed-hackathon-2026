62282ec654e76ee65cdfd98f0a63b9f3e20bba85
hayatosc <hayato8190+univ@gmail.com>
2026-03-21 17:22:36 +0900

    perf(client): change Link prefetch default from intent to none
    Prevents excessive prefetch requests on link-dense pages like the timeline
    where hover/touch intent would saturate the network.
    
    Closes #94

--- application/app/components/foundation/Link.tsx
+++ application/app/components/foundation/Link.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 import { Link as RouterLink, type LinkProps } from "react-router";
 
 export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ prefetch = "intent", ...props }, ref) => {
+  ({ prefetch = "none", ...props }, ref) => {
     return <RouterLink ref={ref} prefetch={prefetch} {...props} />;
   },
 );
